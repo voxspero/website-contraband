7b63a3b3f633aff5eea9fc223255b220e43810fd
voxspero <jbernhardt7@gmail.com>
2020-01-24 10:18:38 -0500

    add adobe web typekit

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,43 +1,4 @@
-let spaceNotes = () => {
-	if ($('p').length > 1) {
-		for (let i = 0; i < $('p').length - 1; i++) {
-			let currentElement = $('p').eq(i);
-			currentElement.addClass('notepad__note--space');
-		}
-	}
-};
+// jQuery scripts
+$(document).ready(() => {});
 
-$(document).ready(() => {
-	let empty = 'Nothing here yet.';
-
-	spaceNotes();
-
-	$('#submit').on('click', (e) => {
-		e.preventDefault();
-
-		if ($('#notes').html().includes(empty)) {
-			$('#notes').html('');
-		}
-
-		let input = $('#input').val();
-
-		$('#notes').append('<p>' + input + '</p>');
-
-		$('p').addClass('notepad__note');
-
-		spaceNotes();
-
-		$('#input').val('');
-	});
-
-	// $('.form__btn').on('active', () => {
-	// 	$(this).addClass('form__btn--success');
-	// });
-
-	$('#clear').on('click', (e) => {
-		e.preventDefault();
-
-		$('#notes').html('<p>' + empty + '</p>');
-		$('p').addClass('notepad__note--empty');
-	});
-});
+// other scripts
